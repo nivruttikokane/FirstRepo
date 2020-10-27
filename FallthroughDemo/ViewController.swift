535f62bc5d6809a769096425b64408bdcf64e383
Nivrutti Kokane <nivrutti.kokane@neosofttech.com>
2020-10-28 00:36:56 +0530

    First commit

---
+++ FallthroughDemo/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  FallthroughDemo
+//
+//  Created by webwerks on 22/10/20.
+//  Copyright © 2020 webwerks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let age = 10
+        switch age {
+        case 10 :
+        print("10")
+            fallthrough
+    
+        default :
+            print("100")
+    }
+
+
+}
+}
+
